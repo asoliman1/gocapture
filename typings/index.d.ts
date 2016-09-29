06d90270f2e3eccdd9469da2b5ba918f76077ed8
Alex Ciobanu <alex.id.ciobanu@gmail.com>
2016-09-29 09:03:21 +0300

    initial commit

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/rx-lite/index.d.ts" />
+/// <reference path="globals/rx.all/index.d.ts" />
+/// <reference path="globals/rx/index.d.ts" />
