c53a79b1c561b2dae1a75159efded172fc15ea65
Alex Ciobanu <alex.id.ciobanu@gmail.com>
2016-09-29 13:13:49 +0300

    Move to ionic 2.0.0 RC Push in android platform as customizations took place

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/rx-lite/index.d.ts" />
-/// <reference path="globals/rx.all/index.d.ts" />
-/// <reference path="globals/rx/index.d.ts" />
