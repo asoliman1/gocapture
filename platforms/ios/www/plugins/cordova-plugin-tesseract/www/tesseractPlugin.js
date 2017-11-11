58750349351f4855e9dd2f1caaf78f29bde1a69f
Alex Ciobanu <alex.id.ciobanu@gmail.com>
2017-11-11 12:20:12 +0200

    attempt fix

--- platforms/ios/www/plugins/cordova-plugin-tesseract/www/tesseractPlugin.js
+++ platforms/ios/www/plugins/cordova-plugin-tesseract/www/tesseractPlugin.js
@@ -1,12 +1,17 @@
 cordova.define("cordova-plugin-tesseract.TesseractPlugin", function(require, exports, module) {
-var TesseractPlugin = {
-    recognizeText: function (image, language, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeText", [language, image]);
-    },
-    loadLanguage: function (language, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "TesseractPlugin", "loadLanguage", [language]);
-    }
-};
-module.exports = TesseractPlugin;
-
+	var TesseractPlugin = {
+		recognizeText: function (image, language, successCallback, errorCallback) {
+			cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeText", [language, image]);
+		},
+		recognizeWords: function (image, language, successCallback, errorCallback) {
+			cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeWords", [language, image]);
+		},
+		recognizeWordsFromPath: function (imagePath, language, successCallback, errorCallback) {
+			cordova.exec(successCallback, errorCallback, "TesseractPlugin", "recognizeWordsFromPath", [language, imagePath]);
+		},
+		loadLanguage: function (language, successCallback, errorCallback) {
+			cordova.exec(successCallback, errorCallback, "TesseractPlugin", "loadLanguage", [language]);
+		}
+	};
+	module.exports = TesseractPlugin;
 });
