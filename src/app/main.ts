37083d59a73c1fbeb57318d63a0919765cd15fe4
Alex Ciobanu <alex.id.ciobanu@gmail.com>
2018-02-06 09:40:12 +0200

    build fixes

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
-
+import { isDevMode, enableProdMode } from '@angular/core';
+import * as dev from "../config/dev";
+import * as prod from "../config/prod";
+//enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+isDevMode() ? dev.setupConfig() : prod.setupConfig();
