f854ea0b927ebbd4359c8a91b64ce7c40a30e73f
Sergij Rylskyj <sergij.rylskyj@gmail.com>
2018-08-17 20:30:07 +0300

    fixed barcode

--- src/components/form-view/dynamic-form-element/dynamic-form-element.ts
+++ src/components/form-view/dynamic-form-element/dynamic-form-element.ts
@@ -3,6 +3,7 @@ import {FormGroup} from "@angular/forms";
 import {FormElement} from "../../../model/form-element";
 import {FormView} from "../form-view";
 import {Form} from "../../../model/form";
+import {FormSubmission} from "../../../model/form-submission";
 
 @Component({
   selector: 'dynamic-form-element',
@@ -15,6 +16,8 @@ export class DynamicFormElementComponent {
   @Input() submitAttempt: boolean = false;
   @Input() readOnly: boolean = false;
   @Input() form: Form;
+  @Input() submission: FormSubmission;
+
 
   constructor() {
     //
