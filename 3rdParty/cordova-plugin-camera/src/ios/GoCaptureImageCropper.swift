f516c9052d506a825144396a2341ad1ad74052cd
Sergij Rylskyj <sergij.rylskyj@gmail.com>
2018-11-05 10:29:06 +0200

    fixed memory leaks

--- 3rdParty/cordova-plugin-camera/src/ios/GoCaptureImageCropper.swift
+++ 3rdParty/cordova-plugin-camera/src/ios/GoCaptureImageCropper.swift
@@ -21,69 +21,54 @@ extension CGRect {
     }
 }
 
-public typealias CropCompletionHandler = (_ image: UIImage) -> Void;
+public typealias CropCompletionHandler = (_ image: UIImage?) -> Void;
 
 @objc public class GoCaptureImageCropper: NSObject {
-    
-    var completionHandler: CropCompletionHandler?;
-    
-    var inputImage: CIImage!
-    
+
     @objc
     public func crop(uiImage: UIImage, completionHandler: @escaping CropCompletionHandler)   {
-        self.completionHandler = completionHandler;
+
         let ciImage = CIImage(image: uiImage)
         let orientation = CGImagePropertyOrientation(rawValue: UInt32(uiImage.imageOrientation.rawValue))
-        
-        inputImage = ciImage?.oriented(forExifOrientation: Int32(orientation!.rawValue))
+
+        var inputImage = ciImage?.oriented(forExifOrientation: Int32(orientation!.rawValue));
+
         let handler = VNImageRequestHandler(ciImage: ciImage!, orientation: CGImagePropertyOrientation(rawValue: orientation!.rawValue)!)
         DispatchQueue.global(qos: .userInteractive).async {
             do {
-                try handler.perform([self.rectanglesRequest])
-//                let image = UIImage.init(ciImage: self.inputImage)  ;
+                try handler.perform([VNDetectRectanglesRequest(completionHandler: { [weak self] (request, error) in
+                    let handledImage = self?.handleRectangles(request: request, error: error, image: inputImage!);
+                    inputImage = nil;
+                    DispatchQueue.main.async {
+                        completionHandler(handledImage);
+                    }
+                })])
             } catch {
                 print(error)
             }
         }
     }
-    
-    lazy var rectanglesRequest: VNDetectRectanglesRequest = {
-        var request = VNDetectRectanglesRequest(completionHandler: self.handleRectangles);
-        return request;
-    }()
-    
-    func handleRectangles(request: VNRequest, error: Error?) {
+
+    func handleRectangles(request: VNRequest, error: Error?, image: CIImage) -> UIImage? {
         guard let observations = request.results as? [VNRectangleObservation]
             else { fatalError("unexpected result type from VNDetectRectanglesRequest") }
         guard let detectedRectangle = observations.first else {
             print("No rectangles detected.")
-            return
+            return nil;
         }
-        let imageSize = inputImage.extent.size
-        
+        let imageSize = image.extent.size
+
         // Verify detected rectangle is valid.
         let boundingBox = detectedRectangle.boundingBox.scaled(to: imageSize)
-        guard inputImage.extent.contains(boundingBox)
-            else { print("invalid detected rectangle"); return }
-        
-        // Rectify the detected image and reduce it to inverted grayscale for applying model.
-        
-        inputImage = inputImage
-            .cropped(to: boundingBox)
-        // Show the pre-processed image
-        
-        if let handler = self.completionHandler {
-            DispatchQueue.main.async {
-                handler(self.convert(cmage: self.inputImage));
-            }
-        }
+        guard image.extent.contains(boundingBox)
+            else { print("invalid detected rectangle"); return nil }
+
+        return self.convert(cmage: image.cropped(to: boundingBox));
     }
-    
-    func convert(cmage:CIImage) -> UIImage
-    {
+
+    func convert(cmage:CIImage) -> UIImage {
         let context:CIContext = CIContext.init(options: nil)
         let cgImage:CGImage = context.createCGImage(cmage, from: cmage.extent)!
-        let image:UIImage = UIImage.init(cgImage: cgImage)
-        return image
+        return UIImage.init(cgImage: cgImage)
     }
 }
