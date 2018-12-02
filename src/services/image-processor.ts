5488d596c5dfc8328517d8bec556bf057643d68d
Sergij Rylskyj <sergij.rylskyj@gmail.com>
2018-12-02 18:07:20 +0200

    increased margin.

--- src/services/image-processor.ts
+++ src/services/image-processor.ts
@@ -65,7 +65,7 @@ export class ImageProcessor{
 
         let coefficient = image.naturalWidth / self.platform.width();
 
-        let padding = 15;
+        let padding = 25;
 
         let canvasWidth = self.platform.width() * coefficient;
         let aspectRatio = crop.width / crop.height;
