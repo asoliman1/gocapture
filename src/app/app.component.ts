b4fccdff2b0956959fdbc86d51fe2f571a632a28
Ahmed Elsayed <leadliaison@Ahmeds-MacBook-Pro.local>
2019-10-22 16:45:58 +0200

    fixed a bug on app resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -148,7 +148,7 @@ export class MyApp {
   private onAppResumes() {
     this.platform.resume.subscribe(() => {
       // A.S check device status when app resumes
-      if(!this.util.getPluginPrefs() || !this.util.getPluginPrefs('rapid-scan')){
+      if(!(this.util.getPluginPrefs() || this.util.getPluginPrefs('rapid-scan'))){
         this.popup.dismissAll();
         this.checkDeviceStatus();
         this.client.getUpdates().subscribe(() => {
